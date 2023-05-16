2f7d7d99f8128a4031878814d3e8f3f810ff80af
BenjaCoder <ben.hamula@gmail.com>
2023-05-16 15:19:36 -0400

    Add ability to change password length

--- script.js
+++ script.js
@@ -3,8 +3,15 @@ window.onload = () => {
     "U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s",
     "t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#",
     "$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
+    const password1 = document.querySelector("#password1");
+    const password2 = document.querySelector("#password2");
+    const passLengthSlider = document.querySelector("#pass-length");
+    const generatePasswords = document.querySelector("#generate-passwords-btn");
+    const passLengthLabel = document.querySelector("#pass-length-label");
 
-    let passwordLength = 24;
+    passLengthSlider.addEventListener("input", () => {
+        passLengthLabel.textContent = `Characters: ${passLengthSlider.value}`
+    })
 
     function getRandomCharacter() {
         let randomChar = Math.floor(Math.random() * characters.length);
@@ -13,14 +20,14 @@ window.onload = () => {
 
     function generateRandomPassword() {
         let randomPassword = ""
-        for (let i = 0; i < passwordLength; i++) {
+        for (let i = 0; i < passLengthSlider.value; i++) {
             randomPassword += getRandomCharacter();
         }
         return randomPassword;
     }
 
-    document.querySelector("button").addEventListener("click", () => {
-        document.querySelector("#password1").textContent = generateRandomPassword();
-        document.querySelector("#password2").textContent = generateRandomPassword();
+    generatePasswords.addEventListener("click", () => {
+        password1.textContent = generateRandomPassword();
+        password2.textContent = generateRandomPassword();
     });
 }
